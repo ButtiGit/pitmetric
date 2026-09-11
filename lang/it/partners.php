75f581e8da53975a0880fd5904865ea24c776db6
ButtiGit <buttigit@gmail.com>
2026-09-11 17:06:54 +0200

    feat: add Italian partner recruitment copy

---
+++ lang/it/partners.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'eyebrow' => 'Programma partner',
+    'title' => 'Cerchiamo partner ufficiali per lo sviluppo e i primi utilizzi.',
+    'copy' => 'PitMetric viene progettato pensando all’uso reale in pista. Cerchiamo un numero limitato di piloti, team karting, officine e progetti motorsport disponibili a testare il prodotto in anticipo e ad aiutarci a capire cosa conta davvero prima di un rilascio più ampio.',
+    'development_title' => 'Partner di sviluppo',
+    'development_copy' => 'Per sviluppatori, collaboratori tecnici e professionisti motorsport che vogliono contribuire a migliorare flussi, integrazioni e direzione del prodotto.',
+    'early_title' => 'Partner per i primi utilizzi',
+    'early_copy' => 'Per piloti, proprietari e piccoli team disponibili a usare PitMetric su mezzi reali e condividere feedback pratici dal paddock.',
+    'benefit_1' => 'Contatto diretto con lo sviluppatore',
+    'benefit_2' => 'Influenza sulla roadmap prima del rilascio pubblico',
+    'benefit_3' => 'Accesso prioritario alle nuove funzioni',
+    'cta' => 'Parliamo di una partnership',
+    'note' => 'Niente vendita aggressiva: in questa fase cerchiamo soprattutto collaborazioni utili e reali.',
+];
