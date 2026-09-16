6252245c368327f54403b0ab0f21eecf74df5072
ButtiGit <buttigit@gmail.com>
2026-09-16 14:54:19 +0200

    Point 7: add control center routes

---
+++ routes/productization.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\ControlCenterController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'verified', 'database.access'])
+    ->prefix('control-center')
+    ->name('control-center.')
+    ->group(function () {
+        Route::get('/', [ControlCenterController::class, 'index'])->name('index');
+        Route::get('/export', [ControlCenterController::class, 'export'])->name('export');
+        Route::post('/import', [ControlCenterController::class, 'import'])->name('import');
+
+        Route::post('/documents', [ControlCenterController::class, 'storeDocument'])->name('documents.store');
+        Route::get('/documents/{document}/download', [ControlCenterController::class, 'downloadDocument'])->name('documents.download');
+        Route::delete('/documents/{document}', [ControlCenterController::class, 'destroyDocument'])->name('documents.destroy');
+
+        Route::post('/notifications/{notification}/read', [ControlCenterController::class, 'readNotification'])->name('notifications.read');
+        Route::post('/notifications/read-all', [ControlCenterController::class, 'readAllNotifications'])->name('notifications.read-all');
+    });
