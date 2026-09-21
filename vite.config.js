5ca4fbf03b29deb785a6ad75fc71e39939a6fafb
Simone Buttice <buttigit@gmail.com>
2026-09-21 15:33:47 +0200

    Reduce Vite to two surface entrypoints

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/public.css',
                 'resources/js/public.js',
             ],
             refresh: true,
