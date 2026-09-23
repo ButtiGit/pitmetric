885a423e4cf6698c80dd6249398bfbe160c22d4c
Simone Buttice <buttigit@gmail.com>
2026-09-23 15:22:27 +0200

    Annotate mobile token date casts

--- app/Models/MobileAccessToken.php
+++ app/Models/MobileAccessToken.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $token_hash
+ * @property string|null $device_name
+ * @property Carbon|null $last_used_at
+ * @property Carbon|null $expires_at
+ * @property-read User $user
+ */
 #[Fillable(['user_id', 'token_hash', 'device_name', 'last_used_at', 'expires_at'])]
 class MobileAccessToken extends Model
 {
