ed755ed5d0400e5c8481c0f727d2ff50abd5442d
Simone Buttice <buttigit@gmail.com>
2026-09-23 15:23:13 +0200

    Annotate gallery photo model casts

--- app/Models/GalleryPhoto.php
+++ app/Models/GalleryPhoto.php
@@ -6,7 +6,23 @@ use App\Models\Concerns\BelongsToWorkspace;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property int $workspace_id
+ * @property int|null $user_id
+ * @property string $client_uuid
+ * @property string $title
+ * @property string|null $description
+ * @property string $path
+ * @property string $mime_type
+ * @property string|null $original_filename
+ * @property int $size_bytes
+ * @property Carbon|null $taken_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ */
 #[Fillable([
     'user_id',
     'client_uuid',
