44f449d87bc77089426dc580746cad63bce9dd22
Simone Buttice <buttigit@gmail.com>
2026-09-23 12:24:46 +0200

    Use mobile cockpit and bottom navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <x-layouts::app.sidebar :title="$title ?? null">
-    <flux:main data-pm-workspace class="pitmetric-app min-w-0 overflow-x-hidden">
+    <flux:main data-pm-workspace class="pitmetric-app pm-mobile-shell-main min-w-0 overflow-x-hidden">
         <x-pitmetric.workflow-nav />
         <x-pitmetric.follow-up-bell />
         <x-pitmetric.track-capture-feed />
+        <x-pitmetric.mobile-dashboard />
         {{ $slot }}
         <x-pitmetric.quick-capture />
+        <x-pitmetric.mobile-navigation />
     </flux:main>
 </x-layouts::app.sidebar>
