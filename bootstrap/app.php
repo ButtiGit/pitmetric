dd1712c340c5c2ebc4596d2bd7e112fef50f8c80
Simone Buttice <buttigit@gmail.com>
2026-09-23 15:56:55 +0200

    Register mobile API and auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AuthenticateMobileToken;
 use App\Http\Middleware\EnsureDatabaseAccess;
 use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
@@ -10,6 +11,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -18,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('web', [SetLocale::class]);
         $middleware->alias([
             'database.access' => EnsureDatabaseAccess::class,
+            'mobile.auth' => AuthenticateMobileToken::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
