928a3ea8de8b8271bab43044106abcad36fcf6f4
Simone Buttice <buttigit@gmail.com>
2026-09-23 11:13:39 +0200

    Wire track-first capture into app shell

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <x-layouts::app.sidebar :title="$title ?? null">
     <flux:main data-pm-workspace class="pitmetric-app min-w-0 overflow-x-hidden">
         <x-pitmetric.workflow-nav />
+        <x-pitmetric.follow-up-bell />
+        <x-pitmetric.track-capture-feed />
         {{ $slot }}
+        <x-pitmetric.quick-capture />
     </flux:main>
 </x-layouts::app.sidebar>
