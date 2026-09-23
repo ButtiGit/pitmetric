6820ff86da76ed49688f6d91ae7758f392d941f1
Simone Buttice <buttigit@gmail.com>
2026-09-23 12:24:37 +0200

    Load mobile shell assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 import '../css/app.css';
 import '../css/forms.css';
 import '../css/mobile.css';
+import '../css/mobile-shell.css';
 import '../css/onboarding.css';
 import '../css/workspace.css';
 
 import './pitmetric-demo';
 import './onboarding';
 import './workspace-forms';
+import './mobile-shell';
